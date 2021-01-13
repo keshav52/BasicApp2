67a08ebc7faf6642ce29246c204dcd1535a6d1db
keshav52 <73228599+keshav52@users.noreply.github.com>
2021-01-13 14:17:21 +0530

    Init

---
+++ app/src/main/java/com/example/basicapp2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.basicapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(v -> Toast.makeText(MainActivity.this,"Hello there",Toast.LENGTH_SHORT).show());
+    }
+}
